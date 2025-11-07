d0851a05081ea7cd74d955036b31c41dae69bb84
Howards12 <admin@ivtraining.net>
2025-11-07 17:05:30 -0600

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed from '/vibes-digital-media/' to '/'
-})
+  base: "/", // important for vibesdigitalmedia.com root
+});
