9b1397a34161a667b153c286d8ce25a2f99bed59
Howards12 <admin@ivtraining.net>
2025-11-07 15:52:38 -0600

    Add custom domain configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vibes-digital-media/',
+  base: '/', // Changed from '/vibes-digital-media/' to '/'
 })
