b528eaf6e72736246503d5593697cedef0eb3742
Howards12 <admin@ivtraining.net>
2025-12-12 13:11:24 -0600

    Add standalone pages structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-import VibesDigitalMedia from "./App.jsx";
-import BlogList from "./blog/BlogList.jsx";
-import BlogPost from "./blog/BlogPost.jsx";
-
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Main Vibes site (one-page layout) */}
-        <Route path="/" element={<VibesDigitalMedia />} />
-
-        {/* Blog list page */}
-        <Route path="/blog" element={<BlogList />} />
-
-        {/* Single blog post page */}
-        <Route path="/blog/:slug" element={<BlogPost />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
